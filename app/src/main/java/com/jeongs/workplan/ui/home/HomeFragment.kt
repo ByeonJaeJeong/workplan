904100c314ca20db59ff7359a21a29b2f0cd734e
Jeong <qsz78547@naver.com>
2021-03-30 19:37:40 +0900

    update

--- app/src/main/java/com/jeongs/workplan/ui/home/HomeFragment.kt
+++ app/src/main/java/com/jeongs/workplan/ui/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -24,9 +25,18 @@ class HomeFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
 
+
+
+
+
+        //메서드
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+
         return root
     }
+
+
 }
