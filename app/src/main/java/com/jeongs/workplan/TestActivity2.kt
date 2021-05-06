4526293cabc02818dc9a79b40c7f30d8cfd7a26f
Jeong <qsz78547@naver.com>
2021-05-06 20:17:47 +0900

    acitivity test

--- app/src/main/java/com/jeongs/workplan/TestActivity2.kt
+++ app/src/main/java/com/jeongs/workplan/TestActivity2.kt
@@ -2,10 +2,16 @@ package com.jeongs.workplan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class TestActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test2)
+
+        val exit_Btn = findViewById<Button>(R.id.exit_btn)
+        exit_Btn.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
