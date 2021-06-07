a65590ab415c4e5a7c8e570ab11fe458a33d2b69
Jeong <qsz78547@naver.com>
2021-06-07 19:37:59 +0900

    1.LiveData를 통한 실시간 변경(완료)[다른것과 연동시키며 변동사항있을예정] 2.필터 설정 바텀다이얼로그 생성[다이얼로그.xml 과 동작메서드 생성예정

--- app/src/main/java/com/jeongs/workplan/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/jeongs/workplan/ui/dashboard/DashboardFragment.kt
@@ -1,6 +1,8 @@
 package com.jeongs.workplan.ui.dashboard
 
 import android.os.Bundle
+import android.os.LocaleList
+import android.support.v4.app.INotificationSideChannel
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,21 +11,30 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.jeongs.workplan.R
 import com.jeongs.workplan.ui.home.SharedViewModel
 import kotlinx.android.synthetic.main.fragment_dashboard.*
+import kotlinx.android.synthetic.main.fragment_dashboard.view.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class DashboardFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
     private lateinit var sharedViewModel : SharedViewModel
+    private lateinit var select_cal : Calendar
+    var last_day :Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activity?.run {
             sharedViewModel= ViewModelProvider(this,ViewModelProvider.NewInstanceFactory()).get(SharedViewModel::class.java)
         }
-
+        select_cal = Calendar.getInstance()
+        select_cal.set(sharedViewModel.year,sharedViewModel.month,1)
+        last_day= select_cal.getActualMaximum(Calendar.DAY_OF_MONTH)
     }
 
     override fun onCreateView(
@@ -36,9 +47,32 @@ class DashboardFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
 
         var nStmt_yearEnd_textView= root.findViewById<TextView>(R.id.nStmt_yearEnd_textView)
+        val sdf =SimpleDateFormat("yyyy-MM-dd(E)", Locale.KOREAN)
+        val lastDay_cal = Calendar.getInstance()
+        lastDay_cal.set(sharedViewModel.year,sharedViewModel.month,last_day)
+        dashboardViewModel.text.value=sdf.format(select_cal.time)+" - "+sdf.format(lastDay_cal.time)
         dashboardViewModel.text.observe(viewLifecycleOwner,{
             nStmt_yearEnd_textView.text=it
         })
+
+        root.nStmt_leftButton_textView.setOnClickListener {
+            select_cal.add(Calendar.MONTH,-1)
+            lastDay_cal.add(Calendar.MONTH,-1)
+            last_day=lastDay_cal.getActualMaximum(Calendar.DAY_OF_MONTH)
+            lastDay_cal.set(Calendar.DAY_OF_MONTH,last_day)
+            dashboardViewModel.text.value= sdf.format(select_cal.time)+" - "+sdf.format(lastDay_cal.time)
+        }
+        root.nStmt_rightButton_textView.setOnClickListener {
+            select_cal.add(Calendar.MONTH,1)
+            lastDay_cal.add(Calendar.MONTH,1)
+            last_day=lastDay_cal.getActualMaximum(Calendar.DAY_OF_MONTH)
+            lastDay_cal.set(Calendar.DAY_OF_MONTH,last_day)
+            dashboardViewModel.text.value= sdf.format(select_cal.time)+" - "+sdf.format(lastDay_cal.time)
+        }
+        root.nStmt_yearEnd_textView.setOnClickListener {
+            val bottomDialog = PeriodFilterBottomSheet()
+            activity?.supportFragmentManager?.let { it1 -> bottomDialog.show(it1, bottomDialog.tag)}
+            }
 
 
         return root
---
+++ app/src/main/java/com/jeongs/workplan/ui/dashboard/PeriodFilterBottomSheet.kt
@@ -0,0 +1,21 @@
+package com.jeongs.workplan.ui.dashboard
+
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.jeongs.workplan.R
+
+class PeriodFilterBottomSheet() : BottomSheetDialogFragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.bottom_sheet,container, false)
+        return view
+    }
+
+}
